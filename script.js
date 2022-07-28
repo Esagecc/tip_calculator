a1f8b9e96b8a792fd60f610eecf24582698243ff
Elizabeth Cantu <e.sage.cc@gmail.com>
2022-07-27 19:05:10 -0500

    updating the files for corrections

---
+++ script.js
@@ -0,0 +1,7 @@
+let billAmount = document.querySelector('.left input')
+let button = document.querySelector('button')
+
+// button.addEventListener('click', testing)
+
+
+console.log(billAmount.value)
