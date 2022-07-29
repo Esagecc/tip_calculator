176a0a4e981056a53a81e5dc1eeff95da7633286
Elizabeth Cantu <e.sage.cc@gmail.com>
2022-07-29 01:45:38 -0500

    updated some JS code. Pending reset button and other features

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
-let billAmount = document.querySelector('.left input')
-let button = document.querySelector('button')
 
-// button.addEventListener('click', testing)
+let billAmount = document.getElementById('bill')
+let people = document.getElementById('people')
+let tipNums = document.getElementsByName("tip")
+let custom = document.getElementById('custom')
+let errorMsg = document.getElementById('peopleError')
+let resetBtn = document.getElementById("reset")
+
+billAmount.addEventListener('input', getBill)
+people.addEventListener('input', numPeo)
+custom.addEventListener('input', customTip)
+
+function getBill() {
+    return billAmount.value
+    console.log(billAmount.value)
+}
+
+function numPeo(){
+    console.log(people.value)
+    return people.value
+}
+
+function customTip(){
+    let customTip = document.getElementById('custom').value;
+    console.log(custom)
+}
+
+function getTip(percentage){
+    let final = getBill()
+    let finalPeople = numPeo()
+
+    let tipAmount = (final * percentage / finalPeople)
+    console.log(tipAmount)
+
+    let totalPerson = ((final/finalPeople) + tipAmount);
+    console.log(totalPerson)
+
+    document.querySelector('#one').innerText = tipAmount.toFixed(2)
+    document.querySelector('#two').innerText = totalPerson.toFixed(2)
 
 
-console.log(billAmount.value)
+}
+
+function errorMessage() {
+
+    if (people === '' && billAmount > 0){
+        document.querySelector('.error').style.display = 'block';
+        }
+}
