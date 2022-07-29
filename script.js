ecbe34864ccbc5378b8ca944e3008e2662d3265f
Elizabeth Cantu <e.sage.cc@gmail.com>
2022-07-29 03:11:17 -0500

    completed the error message and reset button. Assignment done. ;)

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ let errorMsg = document.getElementById('peopleError')
 let resetBtn = document.getElementById("reset")
 
 billAmount.addEventListener('input', getBill)
+billAmount.addEventListener('input', errorMessage)
 people.addEventListener('input', numPeo)
 custom.addEventListener('input', customTip)
+custom.addEventListener('input', customBillTip)
+resetBtn.addEventListener('click', buttonReset)
 
 function getBill() {
     return billAmount.value
@@ -22,7 +25,26 @@ function numPeo(){
 
 function customTip(){
     let customTip = document.getElementById('custom').value;
-    console.log(custom)
+    customTip = customTip/100
+    console.log(customTip)
+}
+
+function customBillTip(){
+    let customTip = document.getElementById('custom').value;
+    customTip = customTip/100
+    console.log(customTip)
+
+    let final = getBill()
+    let finalPeople = numPeo()
+
+    let tipAmount = (final * customTip / finalPeople)
+    console.log(tipAmount)
+
+    let totalPerson = ((final/finalPeople) + tipAmount);
+    console.log(totalPerson)
+
+    document.querySelector('#one').innerText = tipAmount.toFixed(2)
+    document.querySelector('#two').innerText = totalPerson.toFixed(2)
 }
 
 function getTip(percentage){
@@ -37,13 +59,16 @@ function getTip(percentage){
 
     document.querySelector('#one').innerText = tipAmount.toFixed(2)
     document.querySelector('#two').innerText = totalPerson.toFixed(2)
-
-
 }
 
 function errorMessage() {
-
-    if (people === '' && billAmount > 0){
-        document.querySelector('.error').style.display = 'block';
+    if (billAmount.value > 0 && people.value === ''){
+        document.querySelector('#peopleError').style.visibility = 'visible'
         }
+}
+
+errorMessage()
+
+function buttonReset() {
+    location.reload();
 }
